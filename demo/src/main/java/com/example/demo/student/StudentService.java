7af0451826ccdc97942aa4339f6117b61882c201
ehitimum <ehitimumchoudhury@gmail.com>
2023-07-27 19:21:33 +0600

    ADD:1.2

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -2,9 +2,14 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
+import jakarta.transaction.TransactionScoped;
+
+// import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -16,5 +21,45 @@ public class StudentService {
 
     public List<Student> getStudent(){
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalAccessError("Email is taken");
+        }
+        studentRepository.save(student);
+       
+    }
+
+    public void deleteStudentByID(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists){
+            throw new IllegalStateException("Student with id-" + studentId + " does not exist.");
+        }
+        studentRepository.deleteById(studentId);
+    }
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+        .orElseThrow(()-> new IllegalStateException("Student with id-" + studentId + " does not exist."));
+        
+        boolean nameIsValied = (name != null && name.length() > 0);
+        boolean nameIsSame = Objects.equals(student.getName(), name);
+
+        if((nameIsValied && !nameIsSame)==true){
+            student.setName(name);
+        }
+
+        boolean emailIsValied = (email != null && email.length() > 0);
+        boolean emailIsSame = Objects.equals(student.getEmail(), email);
+
+        if((!emailIsSame && emailIsValied)==true){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalAccessError("Email is taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
